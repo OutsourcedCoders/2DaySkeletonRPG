f5a451518ff57871f578bc4ca8b1a7e71dadf561
Jasonface900 <jorge.floresm900@gmail.com>
2018-02-06 13:05:43 -0600

    wew 2: electric boogaloo

--- src/TwoDaySkeletonRPG/Launcher.java
+++ src/TwoDaySkeletonRPG/Launcher.java
@@ -4,6 +4,7 @@ public class Launcher{
 	public static void main(String[] args){
 		//TODO: make some dank codes.
 		System.out.println("This will be the launcher for the skelerpg");
+		System.out.println("Wew.");
 		
 	}
 

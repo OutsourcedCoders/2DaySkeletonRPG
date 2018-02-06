c8740bf3c627fff67182f8eb4f748e6456f018cf
Jasonface900 <jorge.floresm900@gmail.com>
2018-02-06 12:50:13 -0600

    wew

--- src/TwoDaySkeletonRPG/Launcher.java
+++ src/TwoDaySkeletonRPG/Launcher.java
@@ -3,6 +3,8 @@ package TwoDaySkeletonRPG;
 public class Launcher{
 	public static void main(String[] args){
 		//TODO: make some dank codes.
+		System.out.println("This will be the launcher for the skelerpg");
+		
 	}
 
 }

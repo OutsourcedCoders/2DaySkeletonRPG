d849f753b95d513ba6966fc370d2338090ca10c0
Jasonface900 <jorge.floresm900@gmail.com>
2018-02-06 09:53:52 -0600

    some stuff

---
+++ src/TwoDaySkeletonRPG/Entities/Entity.java
@@ -0,0 +1,10 @@
+package TwoDaySkeletonRPG.Entities;
+
+class Entity{
+	private int x, y;
+	private String name;
+	
+	
+
+
+}
---
+++ src/TwoDaySkeletonRPG/Launcher.java
@@ -0,0 +1,8 @@
+package TwoDaySkeletonRPG;
+
+public class Launcher{
+	public static void main(String[] args){
+		//TODO: make some dank codes.
+	}
+
+}
